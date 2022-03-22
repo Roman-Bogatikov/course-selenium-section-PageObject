311f7d9243b84d98012d09667b93e5942e8d8f42
Roman Bogatikov <fox1323@mail.ru>
2022-03-22 13:01:06 +0300

    changing main_page.py and test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,11 @@
 import time
 
+from pages.main_page import MainPage
 
-def test_find_button_addbusket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    browser.get(link)
-    browser.implicitly_wait(10)
-    button = browser.find_elements_by_css_selector('.btn-add-to-basket')
-    assert button, 'Button not found'
 
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
     time.sleep(5)
