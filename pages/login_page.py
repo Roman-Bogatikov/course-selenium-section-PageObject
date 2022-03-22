ee9c4770377b64f311cb104b3cf9bd4507821cd5
Roman Bogatikov <fox1323@mail.ru>
2022-03-22 15:09:57 +0300

    changing login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert 'login' in self.browser.current_url
+        assert 'login' in self.browser.current_url, "url not login page"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
