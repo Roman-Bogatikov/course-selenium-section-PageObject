ce00eddbd7ed6af6ab3c110559e9fd2bb2221735
Roman Bogatikov <fox1323@mail.ru>
2022-03-22 14:33:32 +0300

    changing files for lesson 4.2 step 8

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,4 +18,4 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
